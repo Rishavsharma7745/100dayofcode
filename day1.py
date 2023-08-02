cea84c33f67c0d23c9a429638a35806f9e3de2f9
Rishav Sharma <rishavsharma7745@gmail.com>
2023-08-02 21:52:12 +0530

    first day completed

---
+++ day1.py
@@ -0,0 +1,36 @@
+#This is our first day
+
+#How to print a msg
+
+print("Hello world!")
+
+#giveing tab to a line using \n
+
+print("Rishav\nSharma")
+
+#concatnating the strings 
+
+print("Rishav" + " " + "Sharma")
+
+#input function ie to enter an input
+
+print("Welcom " + input("Enter your name\n") + "on our website")
+
+#excercise swaping numbers
+
+a = input("a: ") 
+b = input("b: ") 
+
+c = a  
+a = b
+b = c  
+
+print("a = " + a)
+print("b = " + b)
+
+
+#first day assignment
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
